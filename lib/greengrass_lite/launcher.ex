a2eeae7ddc255e0c093bf8887e6c9a97d5a9d21f
Jose Rodriguez <jose.rodriguez@intuitivo.com>
2026-05-03 13:12:36 -0300

    backoff

--- lib/greengrass_lite/launcher.ex
+++ lib/greengrass_lite/launcher.ex
@@ -5,6 +5,7 @@ defmodule GreenGrassLite.Launcher do
   require Logger
 
   @dynamic_sup GreenGrassLite.DaemonDynamicSupervisor
+  @max_poll_backoff_ms 60_000
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -22,8 +23,10 @@ defmodule GreenGrassLite.Launcher do
   @impl true
   def init(_opts) do
     schedule_poll(0)
-    {:ok, %{}}
+    {:ok, default_state()}
   end
+
+  defp default_state, do: %{poll_backoff_ms: nil}
 
   @impl true
   def handle_cast(:wake, state) do
@@ -33,24 +36,34 @@ defmodule GreenGrassLite.Launcher do
 
   @impl true
   def handle_info(:poll, state) do
-    poll_interval = Application.get_env(:greengrass_lite, :credentials_poll_interval_ms, 3_000)
+    base_ms = Application.get_env(:greengrass_lite, :credentials_poll_interval_ms, 3_000)
 
-    cond do
-      not GreenGrassLite.Control.enabled?() ->
-        _ = stop_daemons_tree()
+    {next_state, schedule_ms} =
+      cond do
+        not GreenGrassLite.Control.enabled?() ->
+          _ = stop_daemons_tree()
+          {%{state | poll_backoff_ms: nil}, base_ms}
 
-      not GreenGrassLite.Credentials.ready?() ->
-        _ = stop_daemons_tree()
+        not GreenGrassLite.Credentials.ready?() ->
+          _ = stop_daemons_tree()
+          {%{state | poll_backoff_ms: nil}, base_ms}
 
-      true ->
-        case ensure_daemons_running() do
-          :ok -> :ok
-          {:error, reason} -> Logger.warning("GREENGRASS_LITE_LAUNCHER_START_FAILED #{inspect(reason)}")
-        end
-    end
+        true ->
+          case ensure_daemons_running() do
+            :ok ->
+              {%{state | poll_backoff_ms: nil}, base_ms}
 
-    schedule_poll(poll_interval)
-    {:noreply, state}
+            {:error, reason} ->
+              Logger.warning("GREENGRASS_LITE_LAUNCHER_START_FAILED #{inspect(reason)}")
+              prev = state.poll_backoff_ms || base_ms
+              # Back off when spawn fails (e.g. :eagain / EMFILE) to avoid log + retry storms.
+              next_backoff = min(@max_poll_backoff_ms, max(base_ms, trunc(prev * 1.5)))
+              {%{state | poll_backoff_ms: next_backoff}, next_backoff}
+          end
+      end
+
+    schedule_poll(schedule_ms)
+    {:noreply, next_state}
   end
 
   defp schedule_poll(ms) do
